b28850be0ab9f0a02ec8689e16c920d21e285703
Christopher Hiller <boneskull@boneskull.com>
2017-01-09 23:47:23 -0800

    fix palette label and category

--- nodes/flux-bulb.editor.js
+++ nodes/flux-bulb.editor.js
@@ -5,12 +5,13 @@
   const RED = global.RED;
 
   RED.nodes.registerType('flux-bulb', {
-    category: 'output',
+    category: 'flux lighting',
     icon: 'light.png',
     color: '#39b04b',
     align: 'right',
     inputs: 1,
     outputs: 1,
+    paletteLabel: 'flux',
     label () {
       return this.name || 'flux';
     },
